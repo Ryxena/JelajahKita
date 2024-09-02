b41f0bebbe46a1dec773974b76e07b8fea69e426
Ryxena <rubbenme@proton.me>
2024-09-02 20:43:12 +0700

    Fix Destination.php

--- app/Models/Destination.php
+++ app/Models/Destination.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Destination extends Model
@@ -23,5 +24,10 @@ class Destination extends Model
     public function images(): HasMany
     {
         return $this->hasMany(ImageDestination::class);
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, 'category_destinations', 'destination_id', 'category_id');
     }
 }
